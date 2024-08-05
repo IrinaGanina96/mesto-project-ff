efd7c53ca7f69dedff5d1d522feb5db1214d2fb2
IrinaGanina96 <elirg@yandex.ru>
2024-08-06 00:12:26 +0300

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -12,35 +12,23 @@ const container = document.querySelector('.content');
 const placeContainer = container.querySelector('.places__list');
 const addButton = container.querySelector('.profile__add-button');
 
-function addPlase(name, link) {
+initialCards.forEach(function(element) {
     const cardTemplate = document.querySelector('#card-template').content;
     const placeElement = cardTemplate.querySelector('.places__item').cloneNode(true);
-    placeElement.querySelector('.card__title').textContent = name;  
-    placeElement.querySelector('.card__image').src = link;
-    placeElement.querySelector('.card__image').setAttribute('alt', name);
-    placeContainer.append(placeElement);
-
-    const deleteButton = placeElement.querySelector('.card__delete-button');
-    deleteButton.addEventListener('click', delButton)
- 
-    return placeElement;
-}
-
-function delButton(evt) {
+    function deleteCard(evt) {
     evt.target.closest('.card').remove();
-};
-
-addButton.addEventListener('click', function () {
-    const name = document.querySelector('.popup__input_type_url');
-    const link = document.querySelector('.popup__input_type_card-name');
-    addPlase(name.value, link.value);
-  });
-;
-
-initialCards.forEach(function(placeElement) {
-    placeContainer.append(addPlase(placeElement.name, placeElement.link))
+    };
+    
+    placeElement.querySelector('.card__title').textContent = element.name;
+    placeElement.querySelector('.card__image').src = element.link;
+    placeElement.querySelector('.card__image').setAttribute('alt', element.name);
+    
+    const deleteButton = placeElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', deleteCard)
+    
+    placeContainer.append(placeElement)
+    return placeElement;
 })
-
 
 
 
